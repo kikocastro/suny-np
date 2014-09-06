2e909146934fa6cb0cf1156fb0edfb4ad3e87727
Kiko Castro <castro.112358@gmail.com>
2014-09-06 16:07:30 +0000

    table formatted

--- fall2014/algorithms/week2 - ordering/averageRuntimeXSort.rb
+++ fall2014/algorithms/week2 - ordering/averageRuntimeXSort.rb
@@ -21,8 +21,8 @@ def generateRandomPermutation(n)
 end
 
 def bSort(list)
-  
   size = list.length
+  @comparisonsCounter = 0
   
   unless size == 1
     pass = 1
@@ -32,6 +32,7 @@ def bSort(list)
       swap = false
       
       0.upto(size - 1 - pass) do |i|
+        @comparisonsCounter += 1
         if list[i] > list[i+1] 
           list.swap!(i, i+1)
           swap = true
@@ -46,16 +47,52 @@ end
 
 def insertionSort(list)
   size = list.length
+  @comparisonsCounter = 0
   
   1.upto(size - 1) do |i|
     temp = list[i]
     j = i - 1
     
     while j >= 0 && list[j] > temp do
+      @comparisonsCounter += 1
       list[j+1] = list[j]
       j -= 1
     end 
+
     list[j + 1] = temp
   end
-  p list
+  list
 end
+
+
+def estimateAverageRuntime(xSort)
+  results = []
+  (1..100).each do |n|
+    total = 0
+    
+    3.times do |i|
+      randomList = generateRandomPermutation(n)
+      send(xSort, randomList)
+      total += @comparisonsCounter
+    end
+    averageComparisons = total.to_f/3
+    results << [ n, averageComparisons ]
+  end
+  results
+end
+
+def printAverageRuntimeTable(xSort)
+  results = estimateAverageRuntime(xSort)
+  puts xSort
+  puts "   n       av     av/n    av/n^2    av/nlogn    "
+
+  results.each do |result|
+    n = result[0]
+    average = result[1].to_f
+    printf "%7d   %10.2f  %8.4f  %8.4f  %8.5f" %
+     [ n, average, average/n, average/(n*n), average/(n*Math.log2(n))]
+    puts " " 
+  end 
+end
+
+printAverageRuntimeTable("bSort")
