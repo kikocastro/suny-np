df1e2466a8ee9d87b9b9a4eb2c1d1f9364fbf3de
Frederico Castro <castro.112358@gmail.com>
2014-09-20 18:44:56 -0400

    initial

---
+++ fall2014/algorithms/week4 - bissection/bissection.rb
@@ -0,0 +1,11 @@
+# Implements the bisection method for finding a root of f (x) = 0
+# Input: Two real numbers a and b, a < b,
+# a continuous function f (x) on [a, b], f (a)f (b) < 0,
+# an upper bound on the absolute error tolerance > 0,
+# an upper bound on the number of iterations maxIterations
+# Output: An approximate (or exact) value x of a root in (a, b)
+# or an interval bracketing the root if the iteration number limit is reached
+
+def Bisection(function,a,b,tolerance,maxIterations)
+  
+end
