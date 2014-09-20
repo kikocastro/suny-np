32f0121289134d84b546c6e263ee1a9e9633a8c8
Frederico Castro <castro.112358@gmail.com>
2014-09-20 19:09:27 -0400

    has errors

--- fall2014/algorithms/week4 - bissection/bissection.rb
+++ fall2014/algorithms/week4 - bissection/bissection.rb
@@ -6,6 +6,29 @@
 # Output: An approximate (or exact) value x of a root in (a, b)
 # or an interval bracketing the root if the iteration number limit is reached
 
-def Bisection(function,a,b,tolerance,maxIterations)
-  
+def sin(x)
+  Math.sin(x)
 end
+
+def bisection(a,b,tolerance,maxIterations)
+  n = 1
+
+  while n < maxIterations do
+    x = (a + b)/2
+p n
+    return x if (x - a).abs < tolerance
+    functionValue = sin(x)
+    return x if functionValue < 0
+
+    if functionValue * sin(a) < 0
+      p 'oi1'
+      b = x
+    else
+      p 'oi2'
+      a = x
+    end
+    n += 1
+  end
+end
+
+bisection(3, 4, 0.00000001, 1000)
