36e8b7eb0964876a7dbfa2700d9008dd6dcd3ba7
Frederico Castro <castro.112358@gmail.com>
2014-09-20 22:48:55 -0400

    ok

--- fall2014/algorithms/week4 - bissection/bissection.rb
+++ fall2014/algorithms/week4 - bissection/bissection.rb
@@ -6,29 +6,30 @@
 # Output: An approximate (or exact) value x of a root in (a, b)
 # or an interval bracketing the root if the iteration number limit is reached
 
-def sin(x)
+def f(x)
   Math.sin(x)
 end
 
 def bisection(a,b,tolerance,maxIterations)
-  n = 1
+  n = 0
 
   while n < maxIterations do
-    x = (a + b)/2
-p n
+    x = (a + b)/2.0
     return x if (x - a).abs < tolerance
-    functionValue = sin(x)
-    return x if functionValue < 0
+    functionValue = f(x)
+    return x if functionValue == 0
 
-    if functionValue * sin(a) < 0
-      p 'oi1'
+    if functionValue * f(a) < 0
       b = x
     else
-      p 'oi2'
       a = x
     end
     n += 1
   end
 end
 
-bisection(3, 4, 0.00000001, 1000)
+estimatedPi = bisection(2, 4, 0.0000001, 1000).round(9)
+puts "Real Value:       #{ Math::PI.round(9) } "
+puts "Estimatedd Value: #{ estimatedPi } "
+puts "The error begins at the 8th decimal place, accordingly to the expected by the tolerance"
+puts "Error: #{ (Math::PI.round(9) - estimatedPi).abs.round(9) } < tolerance = 1.0e-7"
